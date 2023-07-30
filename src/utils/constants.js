a86b7de795b6ee570baa6e3cc3b3f902fe4df4c8
soldiers740 <129625171+soldiers740@users.noreply.github.com>
2023-07-30 21:55:02 +0530

    removed js mastery

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -13,10 +13,10 @@ import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
 
 export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+ // { name: 'JS Mastery', icon: <CodeIcon />, },//
 
 export const categories = [
   { name: 'New', icon: <HomeIcon />, },
-  { name: 'JS Mastery', icon: <CodeIcon />, },
   { name: 'Coding', icon: <CodeIcon />, },
   { name: 'ReactJS', icon: <CodeIcon />, },
   { name: 'NextJS', icon: <CodeIcon />, },
